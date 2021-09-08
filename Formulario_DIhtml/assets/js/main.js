d69be63310dc32447b462885bb80a2b80f084a69
cleversoncr <cleversoncr@gmail.com>
2021-09-07 23:35:42 -0300

    Inicio do modal para cadastro de prize

--- Formulario_DIhtml/assets/js/main.js
+++ Formulario_DIhtml/assets/js/main.js
@@ -1,6 +1,26 @@
 /* Contador de caracteres */
-function countChars(obj){
-  document.getElementById("charNum").innerHTML = obj.value.length+' caracteres';
+function countChars500(obj){
+  document.getElementById("charNum500").innerHTML = obj.value.length;
+}
+
+function countChars60(obj){
+  document.getElementById("charNum60").innerHTML = obj.value.length;
+}
+function countChars602(obj){
+  document.getElementById("charNum602").innerHTML = obj.value.length;
+}
+
+function countChars65(obj){
+  document.getElementById("charNum65").innerHTML = obj.value.length;
+}
+
+function countChars200(obj){
+  document.getElementById("charNum200").innerHTML = obj.value.length;
+}
+
+
+function countChars28(obj){
+  document.getElementById("charNum28").innerHTML = obj.value.length;
 }
 
 /* Gerador de ID */
@@ -17,3 +37,22 @@ function generateUniqueString() {
 }
 
 document.getElementById("unique").innerHTML = generateUniqueString();
+
+/* Modal Cadastro de prizes */
+let modal = document.getElementById('contact-modal'),
+    openModal = document.getElementById('modal-open'),
+    closeModal = document.querySelector('.close-modal');
+
+openModal.addEventListener('click', function() {
+  modal.style.display = 'block';
+})
+
+closeModal.addEventListener('click', function() {
+  modal.style.display = 'none';
+})
+
+window.addEventListener('click', function(e) {
+  if(e.target == modal) {
+    modal.style.display = 'none';
+  }
+})
